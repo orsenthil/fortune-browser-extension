f8437c88d4c1a72d67876faff3240f6a523cd00a
Senthil Kumaran <senthil@python.org>
2025-06-04 10:47:25 -0700

    Fixes the quote sanitization.

--- assets/js/quotes.js
+++ assets/js/quotes.js
@@ -4,19 +4,35 @@ var getRandomFortune = function () {
     var defaultQuote = "There are far, far better things ahead than any we leave behind.";
     var defaultAuthor = "C.S.Lewis";
 
+    // Sanitize text while preserving legitimate HTML entities
+    var sanitizeText = function(text) {
+        // First escape any HTML to prevent XSS
+        var escaped = Handlebars.escapeExpression(text);
+        // Then convert common HTML entities back to their readable form
+        return escaped
+            .replace(/&amp;/g, '&')
+            .replace(/&lt;/g, '<')
+            .replace(/&gt;/g, '>')
+            .replace(/&quot;/g, '"')
+            .replace(/&#x27;/g, "'")
+            .replace(/&#x2F;/g, '/')
+            .replace(/&#x60;/g, '`')
+            .replace(/&#x3D;/g, '=');
+    };
+
     var getQuote = function() {
-        /* var quotes = $("#quotes").html(); */
-        /* var template = Handlebars.compile(quotes); */
         var template = Handlebars.templates.quotes;
 
         $.get(quotesUrl, function(response) {
             if (response) {
-                var q = Handlebars.escapeExpression(response.quote);
-                var a = Handlebars.escapeExpression(response.author);
-                $(fortuneDiv).append(template({quote: q, author: a}));
+                var safeQuote = sanitizeText(response.quote);
+                var safeAuthor = sanitizeText(response.author);
+                $(fortuneDiv).append(template({quote: safeQuote, author: safeAuthor}));
             }
         }).fail(function(_) {
-            $(fortuneDiv).append(template({quote:defaultQuote, author:defaultAuthor}))
+            var safeDefaultQuote = sanitizeText(defaultQuote);
+            var safeDefaultAuthor = sanitizeText(defaultAuthor);
+            $(fortuneDiv).append(template({quote: safeDefaultQuote, author: safeDefaultAuthor}))
         });
     };
 
