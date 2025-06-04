81c4b634ea8374e61bf0e9b88abad84f7ebb0b72
Senthil Kumaran <senthil@python.org>
2025-06-04 15:37:32 -0700

    Removed the Handle Bars Dependency.

--- assets/js/quotes.js
+++ assets/js/quotes.js
@@ -4,10 +4,25 @@ var getRandomFortune = function () {
     var defaultQuote = "There are far, far better things ahead than any we leave behind.";
     var defaultAuthor = "C.S.Lewis";
 
+    // HTML escape function
+    var escapeHtml = function(text) {
+        if (typeof text !== 'string') return '';
+        return text
+            .replace(/&/g, '&amp;')
+            .replace(/</g, '&lt;')
+            .replace(/>/g, '&gt;')
+            .replace(/"/g, '&quot;')
+            .replace(/'/g, '&#x27;')
+            .replace(/\//g, '&#x2F;')
+            .replace(/`/g, '&#x60;')
+            .replace(/=/g, '&#x3D;');
+    };
+
     // Sanitize text while preserving legitimate HTML entities
     var sanitizeText = function(text) {
+        if (typeof text !== 'string') return '';
         // First escape any HTML to prevent XSS
-        var escaped = Handlebars.escapeExpression(text);
+        var escaped = escapeHtml(text);
         // Then convert common HTML entities back to their readable form
         return escaped
             .replace(/&amp;/g, '&')
@@ -20,20 +35,54 @@ var getRandomFortune = function () {
             .replace(/&#x3D;/g, '=');
     };
 
-    var getQuote = function() {
-        var template = Handlebars.templates.quotes;
+    var createQuoteElement = function(quote, author) {
+        var section = document.createElement('section');
+        var quoteP = document.createElement('p');
+        var authorP = document.createElement('p');
+        
+        quoteP.className = 'quote';
+        quoteP.textContent = quote;
+        
+        authorP.className = 'quote author';
+        authorP.setAttribute('align', 'right');
+        authorP.textContent = author;
+        
+        section.appendChild(quoteP);
+        section.appendChild(authorP);
+        
+        return section;
+    };
 
-        $.get(quotesUrl, function(response) {
-            if (response) {
-                var safeQuote = sanitizeText(response.quote);
-                var safeAuthor = sanitizeText(response.author);
-                $(fortuneDiv).append(template({quote: safeQuote, author: safeAuthor}));
+    var getQuote = function() {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', quotesUrl, true);
+        xhr.onload = function() {
+            if (xhr.status === 200) {
+                try {
+                    var response = JSON.parse(xhr.responseText);
+                    if (response && response.quote && response.author) {
+                        var safeQuote = sanitizeText(response.quote);
+                        var safeAuthor = sanitizeText(response.author);
+                        var quoteElement = createQuoteElement(safeQuote, safeAuthor);
+                        document.querySelector(fortuneDiv).appendChild(quoteElement);
+                    }
+                } catch (e) {
+                    // If JSON parsing fails, use default quote
+                    var quoteElement = createQuoteElement(defaultQuote, defaultAuthor);
+                    document.querySelector(fortuneDiv).appendChild(quoteElement);
+                }
+            } else {
+                // If request fails, use default quote
+                var quoteElement = createQuoteElement(defaultQuote, defaultAuthor);
+                document.querySelector(fortuneDiv).appendChild(quoteElement);
             }
-        }).fail(function(_) {
-            var safeDefaultQuote = sanitizeText(defaultQuote);
-            var safeDefaultAuthor = sanitizeText(defaultAuthor);
-            $(fortuneDiv).append(template({quote: safeDefaultQuote, author: safeDefaultAuthor}))
-        });
+        };
+        xhr.onerror = function() {
+            // If request fails, use default quote
+            var quoteElement = createQuoteElement(defaultQuote, defaultAuthor);
+            document.querySelector(fortuneDiv).appendChild(quoteElement);
+        };
+        xhr.send();
     };
 
     return {
@@ -43,7 +92,7 @@ var getRandomFortune = function () {
     };
 };
 
-$(function () {
+document.addEventListener('DOMContentLoaded', function() {
     var fortune = new getRandomFortune();
     fortune.init();
 });
